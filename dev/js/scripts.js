d88cc094e4bed54d876b8fc2fffdcb7ad19ace88
Nicola Regattieri <nicregattieri@gmail.com>
2018-07-23 18:48:22 -0300

    adjusting easyzoom

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -287,6 +287,50 @@ catch(e){}
 
   // Scripts Pagina de Produto //
     if (pagProduto.length > 0) {
+
+  			// Controller Image Thumbs, Featured and SuperZoom //
+				$("#___rc-p-id").each(function(index) {
+					var id = $(this).attr("value");
+					var data = "/api/catalog_system/pub/products/search/?fq=productId:"+id+"";
+
+					$.getJSON(data, function(data) {
+						$.each(data, function(key, val) {
+							var elements = val.items[0].images;
+							$(elements).each(function(data, val){
+								// Take Image Thumbs //
+									var myLabel = val.imageLabel;
+									var myImageID = val.imageId;
+									var myImageName = val.imageText;
+									$('<li class="'+myLabel+'"><a href="/arquivos/ids/'+myImageID+'-1014-1524/'+myImageName+'.jpg" data-standard="/arquivos/ids/'+myImageID+'-507-762/'+myImageName+'.jpg"><img src="/arquivos/ids/'+myImageID+'-100-150/'+myImageName+'.jpg" /></a></li>').appendTo('ul.thumbnails');
+								// Take Image Thumbs //
+
+								var myFirst = $('.thumbnails li').first();
+								myFirst.addClass('first-thumb');
+
+								var firstThumbStand = $('.thumbnails li.first-thumb a').attr('data-standard');
+								var firstThumbLink = $('.thumbnails li.first-thumb a').attr('href');
+								var firstThumbEx = $('<a href="'+firstThumbLink+'"><img src="'+firstThumbStand+'"/></a>');
+								firstThumbEx.appendTo('.easyzoom');
+								$('.easyzoom a:first-of-type').nextAll().remove();
+
+								// Instantiate EasyZoom instances
+								var $easyzoom = $('.easyzoom').easyZoom();
+
+								// Setup thumbnails example
+								var api1 = $easyzoom.filter('.easyzoom--with-thumbnails').data('easyZoom');
+								$('.thumbnails').on('click', 'a', function(e) {
+									var $this = $(this);
+									e.preventDefault();
+									// Use EasyZoom's `swap` method
+									api1.swap($this.data('standard'), $this.attr('href'));
+								});
+							});
+						});
+
+					});
+				});
+			// Controller Image Thumbs, Featured and SuperZoom //
+      
     try {
       $document.ready(function() {
       // Script Quantidade de Produtos END. Pego a quantidade de produtos pelo val e jogo na URL do botao.
